555d73d612518a373bb3cc2b9ac969b3e3e9204c
AimeexJB <aimeexjb@gmail.com>
2019-02-04 14:00:59 +0000

    updates

--- script2.js
+++ script2.js
@@ -1,6 +1,8 @@
 var camera, scene, renderer, controls, raycaster;
 var cube;
 
+var objects = [];
+
 var  mouse, INTERSECTED;
 var loader = new THREE.TextureLoader();
 
@@ -8,25 +10,65 @@ init();
 animate();
 
 function init() {
+
+	//--------------------------Adding text to screen--------------------------//
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
+
+	var info = document.createElement( 'div' );
+	info.style.position = 'absolute';
+	info.style.top = '20px';
+	info.style.color = 'white'
+	info.style.width = '100%';
+	info.style.textAlign = 'center';
+	info.innerHTML = 'Interactive Cubes';
+	container.appendChild( info );
+
+	//--------------------------Creating Scene--------------------------//
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera( 60, window.innerWidth/window.innerHeight, 0.1, 100);
 
+	//--------------------------Adding Audio--------------------------//
 	var listener = new THREE.AudioListener();
 	camera.add( listener );
 
+	//--------------------------Adding Raycasting--------------------------//
 	raycaster = new THREE.Raycaster();
 	mouse = new THREE.Vector2();
 
-	raycaster.setFromCamera( mouse, camera );
-
+	//--------------------------Rendering the Scene--------------------------//
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 
+	//--------------------------Adding the Controls to move--------------------------//
 	controls = new THREE.OrbitControls( camera, renderer.domElement );
+	// controls = new THREE.FirstPersonControls( camera );
 
 	camera.position.z = 5;
 
+	//--------------------------Adding the Start Screen--------------------------//
+	var blocker = document.getElementById( 'blocker' );
+	var start = document.getElementById( 'start' );
+
+	start.addEventListener( 'click', function () {
+		controls.lock();
+	}, false );
+
+	controls.addEventListener( 'lock', function () {
+		start.style.display = 'none';
+		blocker.style.display = 'none';
+	} );
+
+	controls.addEventListener( 'unlock', function () {
+		blocker.style.display = 'block';
+		start.style.display = '';
+	} );
+
+	scene.add( controls.getObject() );
+
+
+	//--------------------------Loading the Background Image--------------------------//
 	loader.load('public/tron3.jpg', function(texture){
 		var cubeGeom = new THREE.BoxGeometry(90, 90, 90);
 		var cubeMat = new THREE.MeshBasicMaterial({
@@ -39,6 +81,7 @@ function init() {
 		mesh4.position.set(0,0,0);
 	})
 
+	//--------------------------Adding the Cubes to the Scene--------------------------//
 	for (var i = 0; i < 1500; i++){
 		var geometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 );
 		var material = new THREE.MeshLambertMaterial();
@@ -48,7 +91,10 @@ function init() {
 		cube.position.x = (Math.random()* 100) - 50;
 		cube.position.y = (Math.random()* 100) - 50;
 		scene.add( cube );
+		object.push(cube);
 	}
+
+	//--------------------------Adding The lighting effects--------------------------//
 
 	// var light = new THREE.PointLight(0xffffff, 20, 100);
 	// light.position.set(30,0,0);
@@ -59,6 +105,8 @@ function init() {
 
 
 	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+
 }
 
 function onDocumentMouseMove( event ) {
@@ -69,7 +117,6 @@ function onDocumentMouseMove( event ) {
 	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
 }
-
 
 function animate() {
 
