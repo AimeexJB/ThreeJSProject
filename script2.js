ed825a2d473087352d894315bc021cc5b5db024f
AimeexJB <aimeexjb@gmail.com>
2019-03-31 17:25:40 +0100

    changes for user testing

--- script2.js
+++ script2.js
@@ -14,26 +14,45 @@ var objects = [];
 
 var loader = new THREE.TextureLoader();
 
-var songs = [{"title": "Smells Like Teen Spirit - Nirvana", "source": "music/SmellsLikeTeenSpirit.mp3", "description": "10/10 would listen to"},
-			{"title": "Imagine - John Lennon", "source": "music/Imagine.mp3", "description": "10/10 would listen to"},
-			{"title": "One - U2", "source": "music/One.mp3", "description": "10/10 would listen to"},
-			{"title": "Billie Jean - Michael Jackson", "source": "music/BillieJean.mp3", "description": "10/10 would listen to"},
-			{"title": "Bohemian Rhapsody - Queen", "source": "music/BohemianRhapsody.mp3", "description": "10/10 would listen to"},
-			{"title": "Hey Jude - The Beatles", "source": "music/HeyJude.mp3", "description": "10/10 would listen to"},
-			{"title": "I Can't Get No Satisfaction - Rolling Stones", "source": "music/ICantGetNo.mp3", "description": "10/10 would listen to"},
-			{"title": "Sweet Child O'Mine - Guns N' Roses", "source": "music/SweetChildMine.mp3", "description": "10/10 would listen to"},
-			{"title": "London Calling - The Clash", "source": "music/LondonCalling.mp3", "description": "10/10 would listen to"},
-			{"title": "Hotel California - The Eagles", "source": "music/HotelCalifornia.mp3", "description": "10/10 would listen to"},
-			{"title": "Your Song - Elton John", "source": "music/YourSong.mp3", "description": "10/10 would listen to"},
-			{"title": "Stairway To Heaven - Led Zeppelin", "source": "music/StairwayToHeaven.mp3", "description": "Song from Your Name movie 10/10 would listen to"},
-			{"title": "I Will Always Love You - Whitney Houston", "source": "music/IWillAlwaysLoveYou.mp3", "description": "Song from Your Name movie 10/10 would listen to"},
-			{"title": "Heartbreak Hotel - Elvis Presley", "source": "music/HeartbreakHotel.mp3", "description": "Song from Your Name movie 10/10 would listen to"},
-			{"title": "Over The Rainbow - Judy Garland", "source": "music/OverTheRainbow.mp3", "description": "Song from Your Name movie 10/10 would listen to"},
-			{"title": "What's Goin' On - Marvin Gaye", "source": "music/WhatsGoinOn.mp3", "description": "Song from Your Name movie 0/10 would listen to"},
-			{"title": "Creep - Radiohead", "source": "music/Creep.mp3", "description": "Song from Your Name movie 6/10 would listen to"},
-			{"title": "Bridge Over Troubled Water - Simon & Garfunkel", "source": "music/BridgeOverTroubledWater.mp3", "description": "Song from Your Name movie 6/10 would listen to"},
-			{"title": "Respect - Aretha Franklin", "source": "music/Respect.mp3", "description": "Song from Your Name movie 6/10 would listen to"},
-			{"title": "Dancing Queen - ABBA", "source": "music/DancingQueen.mp3", "description": "Song from Your Name movie 6/10 would listen to"}];
+var songs = [{"title": "Smells Like Teen Spirit - Nirvana", "source": "music/SmellsLikeTeenSpirit.mp3", "description": "Smells Like Teen Spirit is a song by American rock band Nirvana. It is the opening track and lead single from the band's second album, Nevermind (1991), released on DGC Records.Smells Like Teen Spirit was Nirvana's biggest hit, placing high on music industry charts around the world in 1991 and 1992. The unexpected success propelled Nevermind to the top of the charts at the 	start of 1992, an event often marked as the point where grunge entered the mainstream."},
+
+			{"title": "Imagine - John Lennon", "source": "music/Imagine.mp3", "description": "Imagine is a song co-written and performed by English musician John Lennon. The best-selling single of his solo career, its lyrics encourage the listener to imagine a world at peace without the barriers of borders or the divisions of religion and nationality and to consider the possibility that the whole of humanity would live unattached to material possessions. Shortly before his death, Lennon said that much of the song's lyric and content came from his wife Yoko Ono, and in 2017, she received a co-writing credit"},
+
+			{"title": "One - U2", "source": "music/One.mp3", "description": "One is a song by Irish rock band U2. It is the third track from their 1991 album Achtung Baby, and it was released as the record's third single in February 1992."},
+
+			{"title": "Billie Jean - Michael Jackson", "source": "music/BillieJean.mp3", "description": "Billie Jean is a song by American singer Michael Jackson. The track was released by Epic Records on January 2, 1983 as the second single from his sixth studio album, Thriller (1982). It was written and composed by Jackson, who produced it with Quincy Jones."},
+
+			{"title": "Bohemian Rhapsody - Queen", "source": "music/BohemianRhapsody.mp3", "description": "Bohemian Rhapsody is a song by the British rock band Queen. It was written by Freddie Mercury for the band's 1975 album A Night at the Opera. It is a six-minute suite, consisting of several sections without a chorus: an intro, a ballad segment, an operatic passage, a hard rock part and a reflective coda. The song is a more accessible take on the 1970s progressive rock genre."},
+
+			{"title": "Hey Jude - The Beatles", "source": "music/HeyJude.mp3", "description": "Hey Jude is a song by the English rock band the Beatles that was released as a non-album single in August 1968. It was written by Paul McCartney and credited to the Lennon–McCartney partnership. The single was the Beatles' first release on their Apple record label and one of the First Four singles by Apple's roster of artists, marking the label's public launch. Hey Jude was a number-one hit in many countries around the world and became the top-selling single of 1968 in the UK, the US, Australia and Canada. Its nine-week run at number one on the Billboard Hot 100 tied the all-time record in 1968 for the longest run at the top of the US charts. It has sold approximately eight million copies and is frequently included on music critics' lists of the greatest songs of all time."},
+
+			{"title": "I Can't Get No Satisfaction - Rolling Stones", "source": "music/ICantGetNo.mp3", "description": "(I Can't Get No) Satisfaction is a song by the English rock band the Rolling Stones, released in 1965. It was written by Mick Jagger and Keith Richards and produced by Andrew Loog Oldham. Richards three-note guitar riff—‌intended to be replaced by horns—‌opens and drives the song."},
+
+			{"title": "Sweet Child O'Mine - Guns N' Roses", "source": "music/SweetChildMine.mp3", "description": "Sweet Child o' Mine is a song by American rock band Guns N' Roses, appearing on their debut album, Appetite for Destruction. Released in August 1988 as the album's third single, the song topped the Billboard Hot 100 chart, becoming the band's only number 1 US single. Billboard ranked it the number 5 song of 1988. Re-released in 1989, it reached number 6 on the UK Singles Chart."},
+
+			{"title": "London Calling - The Clash", "source": "music/LondonCalling.mp3", "description": "London Calling is the third studio album by English rock band The Clash. It was originally released as a double album in the United Kingdom on 14 December 1979 by CBS Records, and in the United States in January 1980 by Epic Records."},
+
+			{"title": "Hotel California - The Eagles", "source": "music/HotelCalifornia.mp3", "description": "Hotel California is the title track from the Eagles' album of the same name and was released as a single in February 1977. Writing credits for the song are shared by Don Felder (music), Don Henley, and Glenn Frey (lyrics). The Eagles' original recording of the song features Henley singing the lead vocals and concludes with an extended section of electric guitar interplay between Felder and Joe Walsh."},
+
+			{"title": "Your Song - Elton John", "source": "music/YourSong.mp3", "description": "Your Song is a song composed and performed by English musician Elton John with lyrics by his longtime collaborator, Bernie Taupin. It originally appeared on John's self-titled second studio album (which was released in 1970). The song was released in the United States in October 1970 as the B-side to Take Me to the Pilot. Both songs received airplay, but Your Song was preferred by disc jockeys and replaced Take Me to the Pilot as the A-side, eventually making it to number eight on the Billboard chart. The song also peaked at number seven on the UK Singles Chart, as well as charting in the top 10 in several other countries."},
+
+			{"title": "Stairway To Heaven - Led Zeppelin", "source": "music/StairwayToHeaven.mp3", "description": "Stairway to Heaven is a song by the English rock band Led Zeppelin, released in late 1971. It was composed by guitarist Jimmy Page and vocalist Robert Plant for the band's untitled fourth studio album (often called Led Zeppelin IV). It is often referred to as one of the greatest rock songs of all time."},
+
+			{"title": "I Will Always Love You - Whitney Houston ", "source": "music/IWillAlwaysLoveYou.mp3", "description": "I Will Always Love You is a song originally written and recorded in 1973 by American singer-songwriter Dolly Parton. Her country version of the track was released in 1974 as a single and was written as a farewell to her one-time partner and mentor of seven years, Porter Wagoner, following Parton's decision to pursue a solo career. Whitney Houston recorded her version of the song for the 1992 film The Bodyguard. Her single spent 14 weeks at number one on the Billboard Hot 100 chart making it one of the best-selling singles of all time."},
+
+			{"title": "Heartbreak Hotel - Elvis Presley", "source": "music/HeartbreakHotel.mp3", "description": "Heartbreak Hotel is a song recorded by American singer Elvis Presley. It was released as a single on January 27, 1956, Presley's first on his new record label RCA Victor. It was written by Tommy Durden and Mae Boren Axton. A newspaper article about the suicide of a lonely man who jumped from a hotel window inspired the lyrics. Axton presented the song to Presley in November 1955 at a country music convention in Nashville. Presley agreed to record it, and did so on January 10, 1956. Heartbreak Hotel comprises an eight-bar blues progression, with heavy reverberation throughout the track, to imitate the character of Presley's Sun recordings."},
+
+			{"title": "Over The Rainbow - Judy Garland", "source": "music/OverTheRainbow.mp3", "description": "Over the Rainbow is a ballad composed by Harold Arlen with lyrics by Yip Harburg. It was written for the movie The Wizard of Oz and was sung by actress Judy Garland in her starring role as Dorothy Gale. It won the Academy Award for Best Original Song and became Garland's signature song."},
+
+			{"title": "What's Goin' On - Marvin Gaye", "source": "music/WhatsGoinOn.mp3", "description": "What's Going On is a song by American recording artist Marvin Gaye, released in 1971 on the Motown subsidiary Tamla. Originally inspired by a police brutality incident witnessed by Renaldo Obie Benson, the song was composed by Benson, Al Cleveland and Gaye and produced by Gaye himself. The song marked Gayes departure from the Motown Sound towards more personal material. Later topping the Hot Soul Singles chart for five weeks and crossing over to number two on the Billboard Hot 100, it would sell over two million copies, becoming Gayes second-most successful Motown song to date."},
+
+			{"title": "Creep - Radiohead", "source": "music/Creep.mp3", "description": "Creep is a song by the English alternative rock band Radiohead, released as their debut single in 1992. It appeared on their first album, Pablo Honey (1993). Creep was not initially a chart success, but became a worldwide hit after being rereleased in 1993. Radiohead took elements from the 1972 song The Air That I Breathe; following legal action, Albert Hammond and Mike Hazlewood are credited as cowriters."},
+
+			{"title": "Bridge Over Troubled Water - Simon & Garfunkel", "source": "music/BridgeOverTroubledWater.mp3", "description": "Bridge over Troubled Water is the fifth and final studio album by American folk rock duo Simon & Garfunkel, released in January 1970 on Columbia Records. Following the duo's soundtrack for The Graduate, Art Garfunkel took an acting role in the film Catch-22, while Paul Simon worked on the songs, writing all tracks except Felice and Boudleaux Bryant's Bye Bye Love (previously a hit for the Everly Brothers)."},
+
+			{"title": "Respect - Aretha Franklin", "source": "music/Respect.mp3", "description": "Respect is a song written and originally released by American recording artist Otis Redding in 1965. The song became a 1967 hit and signature song for R&B singer Aretha Franklin. The music in the two versions is significantly different, and through a few changes in the lyrics, the stories told by the songs have a different flavor. Redding's version is a plea from a desperate man, who will give his woman anything she wants. He won't care if she does him wrong, as long as he gets his due respect when he brings money home. However, Franklin's version is a declaration from a strong, confident woman, who knows that she has everything her man wants. She never does him wrong, and demands his respect. Franklin's version adds the R-E-S-P-E-C-T chorus and the backup singers' refrain of Sock it to me, sock it to me, sock it to me...."},
+
+			{"title": "Dancing Queen - ABBA", "source": "music/DancingQueen.mp3", "description": "Dancing Queen is a Europop song by the Swedish group ABBA, and the lead single from their fourth studio album, Arrival. It was written by Benny Andersson, Björn Ulvaeus and Stig Anderson. Andersson and Ulvaeus also produced the song. Dancing Queen was released as a single in Sweden on 15 August 1976, followed by a UK release and the rest of Europe a few days later. It was a worldwide hit. It became ABBA's only number one hit in the United States, and topped the charts in Australia, Canada, the Netherlands, Belgium, Ireland, Mexico, New Zealand, Norway, South Africa, Spain, Sweden, the United Kingdom, Germany and Zimbabwe. Dancing Queen also reached the top five in many other countries"}];
 
 
 init();
@@ -118,7 +137,7 @@ function init() {
 
 
 	//--------------------------Adding the Cubes to the Scene--------------------------//
-	for (var i = 0; i < 20; i++){
+	for (var i = 0; i < 500; i++){
 		if (i <= 20) {
 			var geometry = new THREE.BoxGeometry( 5, 5, 5 );
 		} else {
@@ -194,11 +213,15 @@ function onDocumentMouseDown( event ) {
 
 	event.preventDefault();
 
+	// calculate mouse position in normalized device coordinates
+	// (-1 to +1) for both components
 	mouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
 	mouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
 
+	// update the picking ray with the camera and mouse position
 	raycaster.setFromCamera( mouse, camera );
 
+	// calculate objects intersecting the picking ray
 	var intersects = raycaster.intersectObjects( objects, true );
 
 	if ( intersects.length > 0 )
@@ -233,6 +256,9 @@ function onDocumentMouseDown( event ) {
 
 
 function onclick(event) {
+
+	// calculate mouse position in normalized device coordinates
+	// (-1 to +1) for both components
 	mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
 	mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
@@ -264,6 +290,7 @@ function onclick(event) {
 			source.setAttribute("src", songs[intersects[0].object.id -10].source );
 
 			songInfo.innerHTML = songs[intersects[0].object.id -10].description;
+			songInfo.style.margin = 'auto';
 			container.appendChild( boxinfo );
 
 	}
